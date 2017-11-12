9790f55c8d4e6eac7aff0bd90ef91115cb94f51f
Ryan Rapini <ryanrapini@gmail.com>
2017-11-12 01:07:36 -0500

    First iteration

---
+++ src/components/About.vue
@@ -0,0 +1,94 @@
+<template>
+	<div class="hello">
+		<md-layout md-column md-gutter>
+			<md-layout md-align="center">
+				<h1>Meet the Team</h1>
+			</md-layout>
+			<md-layout>
+				<md-layout md-column>
+					<md-card>
+						<md-card-media>
+							<img src="../assets/photos/ryan1.jpg" alt="People">
+						</md-card-media>
+
+						<md-card-header>
+							<div class="md-title">Ryan Rapini</div>
+							<div class="md-subhead">Code Ninja</div>
+						</md-card-header>
+
+						<md-card-content>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+						</md-card-content>
+					</md-card>
+				</md-layout>
+				<md-layout md-column>
+					<md-card>
+						<md-card-media>
+							<img src="../assets/photos/tom1.jpg" alt="Tom">
+						</md-card-media>
+
+						<md-card-header>
+							<div class="md-title">Tom Emelko</div>
+							<div class="md-subhead">Master of Code</div>
+						</md-card-header>
+
+						<md-card-content>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+						</md-card-content>
+					</md-card>
+				</md-layout>
+				<md-layout md-column>
+					<md-card>
+						<md-card-media>
+							<img src="../assets/photos/fritz1.jpg" alt="People">
+						</md-card-media>
+
+						<md-card-header>
+							<div class="md-title">Fritz Janesh</div>
+							<div class="md-subhead">Super Good Drawer-er</div>
+						</md-card-header>
+
+
+						<md-card-content>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+						</md-card-content>
+					</md-card>
+				</md-layout>
+			</md-layout>
+		</md-layout>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'About',
+	data () {
+		return {
+			msg: 'Welcome to Your Vue.js App'
+		}
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+	font-weight: normal;
+}
+
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+
+a {
+	color: #42b983;
+}
+
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="hello">
+		<h1>{{ msg }}</h1>
+		<h2>Essential Links</h2>
+		<md-boards :md-auto="true" :md-infinite="true" :md-duration="5000" :md-swipeable="true">
+			<md-board id="slide1">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+			</md-board>
+
+			<md-board id="slide2">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+			</md-board>
+
+			<md-board id="slide3">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
+			</md-board>
+		</md-boards>
+		<ul>
+			<li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+			<li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+			<li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+			<li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+			<br>
+			<li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+		</ul>
+		<h2>Ecosystem</h2>
+		<ul>
+			<li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+			<li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+			<li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+			<li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Home',
+	data () {
+		return {
+			msg: 'Welcome to Your Vue.js App'
+		}
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+	font-weight: normal;
+}
+
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+
+a {
+	color: #42b983;
+}
+
+
+</style>
